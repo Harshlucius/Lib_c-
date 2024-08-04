22075bdb6c1e834e9f281ebfa3907d26054fc3d9
Harshlucius <Harshluciuscoder@gmail.com>
2024-08-04 14:02:02 +0530

    div has been updated to handle denominator as zero.

--- c++_lib/add-sub-div-mul/adsm.c
+++ c++_lib/add-sub-div-mul/adsm.c
@@ -1,9 +1,17 @@
 // add.c
+#include <stdio.h>
 int add(int a, int b) {
     return a + b;
 }
-int div(int a,int b){
-    return  a/b;
+int div(int a, int b) {
+    if (b == 0) {
+        printf("\n \tDivision by zero is not allowed. Zero will be returned...\n");
+        printf("\t It is recommended to use another number instead of %d\n", b);
+        return 0;
+    }
+    else{
+        return a / b;
+    }
 }
 int sub(int a, int b){
     return a-b;
---
+++ c++_lib/add-sub-div-mul/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "adsm.h"
+int main(){
+    printf("%d", div(21,0));
+    return 0;
+}
